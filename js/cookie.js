c11897f73cf5273560a4f6623892b51fff2b956d
eseo22 <mmu937387@gmail.com>
2021-10-14 16:51:00 +0900

    index 팝업 적용

---
+++ js/cookie.js
@@ -0,0 +1,30 @@
+$("#popup .close").on("click", function(e){
+    e.preventDefault();
+
+    var checked = $("#popup").find("input[type=checkbox]").is(":checked");
+    if(checked) setCookie();
+    $("#popup").hide();
+});
+
+function setCookie(){
+    var today = new Date(); 
+    var date = today.getDate();
+    today.setDate(date + 1);
+    //console.log(today);
+    var duedate = today.toGMTString();
+    //console.log(duedate);
+
+    document.cookie = "popup=done; expires="+duedate;
+
+}
+
+var isCookie = document.cookie.indexOf("popup=done");
+console.log(isCookie);
+
+if(isCookie == -1){
+    $("#popup").show();
+    console.log("쿠키없음");
+}else{
+    $("#popup").hide();
+    console.log("쿠키있음");
+}
--- js/members.js
+++ js/members.js
@@ -1,6 +1,6 @@
 $("input[type=submit]").on("click", function(e){
     if(!isTxt("userid", 5)) e.preventDefault();
-    
+    if(!password("userpw1","userpw2")) e.preventDefault();
 });
 
 //아이디 인증
@@ -28,6 +28,21 @@ function password(name1, name2){
     let pw1 = $("input[name="+name1+"]").val();
     let pw2 = $("input[name="+name2+"]").val();
 
-    let len = 8
-    let eng = /a-zA-Z/
+    let len = 8;
+    let eng = /[a-zA-Z]/;
+    let num = /[0-9]/;
+    let ast = /[~!@#$%^&*\[\]]/;
+
+    if(pw1 === pw2 && pw1.length >= len && eng.test(pw1) && num.test(pw1) && ast.test(pw1)){
+        $("input[name="+name1+"]").parent().find("p").remove();
+        return true;
+
+    }else{
+        $("input[name="+name1+"]").parent().find("p").remove();
+        $("input[name="+name1+"]").parent().append(
+            "<p>영문 대소문자,숫자, 또는 특수문자 둥 1가지 이상 조합하여 8자이상 입력해주세요</p>"
+        );
+        return false;
+
+    }
 }
