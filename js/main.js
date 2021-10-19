2ef0e13f5f461614795e8160837752d607e830dd
eseo22 <mmu937387@gmail.com>
2021-10-19 19:02:35 +0900

    index. header 2depth적용

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+//메뉴에 마우스 호버시 2depth
+$(".gnb>li").on("mouseenter", function(){
+    $(this).find(".sub").show();
+});
+$(".gnb>li").on("mouseleave", function(){
+    $(this).find(".sub").hide();
+});
+
+//포커스 이동시 2depth 활성화
+$(".gnb>li").each(function(index){
+    $(".gnb>li").eq(index).find("a").on("focusin",function(){
+        $(".gnb>li").eq(index).find(".sub").show();
+    });
+    $(".gnb>li").eq(index).find("a").last().on("focusout", function(){
+        $(".gnb>li").eq(index).find(".sub").hide();
+    });
+});
