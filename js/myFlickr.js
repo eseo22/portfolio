48025a10c5306eef407bf2dcfdde48de5c8fd904
eseo22 <mmu937387@gmail.com>
2021-10-28 14:26:47 +0900

    flickr 수정

--- js/myFlickr.js
+++ js/myFlickr.js
@@ -1,88 +1,128 @@
-$.ajax({
-    //url:"https://www.flickr.com/services/rest/?method=flickr.photos.search",
-    url:"https://www.flickr.com/services/rest/?method=flickr.people.getPhotos",
-    dataType: "json",
-    data:{
-        api_key: "c3497ae54a8e80023a954c8815e7b28e",
-        per_page:5,
-        format:"json",
-        nojsoncallback:1,
-        privacy_filter:1,
-        user_id : "82828410@N02"
-        //tags:"landscape"
-    }
-})
 
-.success(function(data){
-    
-    let items = data.photos.photo;
-    console.log(items);
+getList({
+    type: "interest"
+});
 
-    $("#gallery").append("<ul>");
+function getList(opt) {
+    var result_opt = {};
 
-    $(items).each(function(index, data){
-        let text = data.title;        
-        if(!data.title){          
-            text = "No description in this photo"; 
-        }
-        $("#gallery ul")
-            .append(
-                $("<li>")
-                    .append(
-                        $("<div>").append(
-                            $("<a>").attr({
-                                href : "https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_b.jpg"
-                            })
-                            .append(                            
-                                $("<img class='thumb'>").attr({
-                                    src : "https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_m.jpg"
-                                })
-                            ),
-                            $("<p>").text(text),    
-                                                   
-                            $("<div class='profile'>")
-                            .append(
-                                $("<span>").text(data.owner)
-                            )   
-                                                     
-                        )
-                
-                    )
-            )
-    });
-
-    const total = $("#gallery ul li").length;
-    let imgNum = 0;
-
-    $("#gallery img").each(function(index, data){
-        
-        data.onload = function(){
-            imgNum++;
-            console.log(imgNum);
-            if(imgNum === total){   
-                
-                $(".loading").addClass("off");
-
-                new Isotope("#gallery ul",{
-                    itemSelector : "#gallery ul li",
-                    columnWidth: "#galley ul li",                  
-                    transitionDuration: "0.5s"
-                }); 
-
-
+    if (opt.type == "interest") {
+        result_opt = {
+            url: "https://www.flickr.com/services/rest/?method=flickr.interestingness.getList",
+            dataType: "json",
+            data: {
+                api_key: "c3497ae54a8e80023a954c8815e7b28e",
+                per_page: 5,
+                format: "json",
+                nojsoncallback: 1,
+                privacy_filter: 1,
             }
         }
-        
+    }
+    if (opt.type == "search") {
+        result_opt = {
+            url: "https://www.flickr.com/services/rest/?method=flickr.photos.search",
+            dataType: "json",
+            data: {
+                api_key: "c3497ae54a8e80023a954c8815e7b28e",
+                per_page: 5,
+                format: "json",
+                nojsoncallback: 1,
+                privacy_filter: 1,
+                tags: opt.tag
+            }
+        }
+    }
+    if (opt.type == "userid") {
+        result_opt = {
+            url: "https://www.flickr.com/services/rest/?method=flickr.people.getPhotos",
+            dataType: "json",
+            data: {
+                api_key: "c3497ae54a8e80023a954c8815e7b28e",
+                per_page: 5,
+                format: "json",
+                nojsoncallback: 1,
+                privacy_filter: 1,
+                user_id: opt.user_id
+            }
+        }
+    }
+    $.ajax(result_opt)
+
+.success(function (data) {
+
+        let items = data.photos.photo;
+        console.log(items);
+
+        $("#gallery").append("<ul>");
+
+        $(items).each(function (index, data) {
+            let text = data.title;
+            if (!data.title) {
+                text = "No description in this photo";
+            }
+            $("#gallery ul")
+                .append(
+                    $("<li>")
+                        .append(
+                            $("<div>").append(
+                                $("<a>").attr({
+                                    href: "https://live.staticflickr.com/" + data.server + "/" + data.id + "_" + data.secret + "_b.jpg"
+                                })
+                                    .append(
+                                        $("<img class='thumb'>").attr({
+                                            src: "https://live.staticflickr.com/" + data.server + "/" + data.id + "_" + data.secret + "_m.jpg"
+                                        })
+                                    ),
+                                $("<p>").text(text),
+
+                                $("<div class='profile'>")
+                                    .append(
+                                        $("<span>").text(data.owner)
+                                    )
+
+                            )
+
+                        )
+                )
+        });
+
+        const total = $("#gallery ul li").length;
+        let imgNum = 0;
+
+        $("#gallery img").each(function (index, data) {
+
+            data.onload = function () {
+                imgNum++;
+                console.log(imgNum);
+                if (imgNum === total) {
+
+                    $(".loading").addClass("off");
+
+                    new Isotope("#gallery ul", {
+                        itemSelector: "#gallery ul li",
+                        columnWidth: "#galley ul li",
+                        transitionDuration: "0.5s"
+                    });
+
+
+                }
+            }
+
+        });
+
+    })
+
+.error(function (err) {
+        console.error(err);
     });
-    
-})
-
-.error(function(err){
-    console.error(err);
-})
+}
 
 
-$("body").on("click", "#gallery ul li", function(e){
+
+
+
+$("body").on("click", "#gallery ul li", function (e) {
     e.preventDefault();
 
     $(".pop").remove();
@@ -93,13 +133,15 @@ $("body").on("click", "#gallery ul li", function(e){
         .append(
             $("<div class='pop'>")
                 .append(
-                    $("<img>").attr({src:imgSrc}),
+                    $("<img>").attr({ src: imgSrc }),
                     $("<span>").text("close")
                 )
         )
 });
 
-$("body").on("click", ".pop span", function(){
+$("body").on("click", ".pop span", function () {
     $(".pop").remove();
 });
+
+
 
