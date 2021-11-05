159fbeab1b49ac2af15d6ac85ab767993e24e2e0
eseo22 <mmu937387@gmail.com>
2021-11-05 13:10:50 +0900

    main 반응형시작

--- js/cookie.js
+++ js/cookie.js
@@ -3,14 +3,7 @@ $("#popup .wrap span").on("click", function(){
     setCookie();
     $("popup").hide();
 })
-// $("#popup .close").on("click", function(e){
-//     e.preventDefault();
 
-//     var checked = $("#popup").find("span").is(":checked");
-
-//     if(checked) setCookie();
-//     $("#popup").hide();
-// });
 
 function setCookie(){
     var today = new Date(); 
