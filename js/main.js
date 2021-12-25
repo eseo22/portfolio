c16959f4f9ee8ad70d433c05f662696fb78b7c59
eseo22 <mmu937387@gmail.com>
2021-12-25 13:51:42 +0900

    main skip navi추가

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
+//navi 활성화
+const navi= document.querySelectorAll("#navi a");
+console.log(navi);
 
-//메뉴에 마우스 호버시 2depth
-$(".gnb>li").on("mouseenter", function () {
-    $(this).find(".sub").show();
-});
-$(".gnb>li").on("mouseleave", function () {
-    $(this).find(".sub").hide();
+$(navi).each(function(index, el) {
+    $(el).on("focusin", function(){
+        $(this).addClass("on");
+    });
+    $(el).on("focusout", function(){
+        $(this).removeClass("on");
+    });
+
+    console.log(el);
+    console.log(this);
 });
 
-//포커스 이동시 2depth 활성화
+
+//메뉴에 마우스 호버시 2depth활성화, 포커스 이동시 2depth 활성화
 $(".gnb>li").each(function (index) {
+    $(".gnb>li").on("mouseenter", function () {
+        $(this).find(".sub").show();
+    });
+    $(".gnb>li").on("mouseleave", function () {
+        $(this).find(".sub").hide();
+    });
     $(".gnb>li").eq(index).find("a").on("focusin", function () {
         $(".gnb>li").eq(index).find(".sub").show();
     });
